d8547965474562d8840c63cd29bb8b2eeac0b806
Jimmy Easter <jmeaster1@gmail.com>
2017-08-05 16:01:16 -0600

    Final commit. README edited.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Make Change Project
 ## Week 1 homework for Skill Distillery
 
+This program calculates the most efficient method of providing change in $USD when given an item cost and an amount tendered. It satisfies all requirements listed below. I have also included $50 bills and $100 bills.
+
 In the cash register we will calculate the amount of change returned to a customer based on the purchase price and the amount tendered. We will also notify the attendant how many of each piece of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer. Change should be provided using the largest bill and coin denominations as possible. Denominations that are not used should not be displayed.
--- src/MoneyChanger.java
+++ src/MoneyChanger.java
@@ -15,9 +15,10 @@ public class MoneyChanger {
 
 		double changeDue = (amtTend - itemCost);// calculates total change due
 
-		int change = (int) ((amtTend*100) - (100*itemCost));// converts the total change due into cents and casts into an int because I
-												// want the number of whole dollar bills (ex: 5, not 5.12)
-		
+		int change = (int) ((amtTend * 100) - (100 * itemCost));// converts the total change due into cents and casts
+																// into an int because I  want the number 
+																// of whole dollar bills (ex: 5, not 5.12)
+
 		int numHundreds = change / 10000;// determines how many 50 dollar bills
 		change = change % 10000;// determines remaining change for next calculation
 		if (numHundreds == 1) {
@@ -25,7 +26,7 @@ public class MoneyChanger {
 		} else if (numHundreds > 1) {
 			System.out.println(numHundreds + " Hundreds");
 		}
-		
+
 		int numFifties = change / 5000;// determines how many 50 dollar bills
 		change = change % 5000;// determines remaining change for next calculation
 		if (numFifties == 1) {
@@ -33,7 +34,7 @@ public class MoneyChanger {
 		} else if (numFifties > 1) {
 			System.out.println(numFifties + " Fifties");
 		}
-		
+
 		int numTwenties = change / 2000;// determines how many 20 dollar bills
 		change = change % 2000;// determines remaining change for next calculation
 		if (numTwenties == 1) {
@@ -41,7 +42,7 @@ public class MoneyChanger {
 		} else if (numTwenties > 1) {
 			System.out.println(numTwenties + " Twenties");
 		}
-		
+
 		int numTens = change / 1000;// determines how many 10 dollar bills
 		change = change % 1000;// determines remaining change for next calculation
 		if (numTens == 1) {
@@ -49,7 +50,7 @@ public class MoneyChanger {
 		} else if (numTens > 1) {
 			System.out.println(numTens + " Tens");
 		}
-		
+
 		int numFives = change / 500;// determines how many 5 dollar bills
 		change = change % 500;// determines remaining change for next calculation
 		if (numFives == 1) {
@@ -57,7 +58,7 @@ public class MoneyChanger {
 		} else if (numFives > 1) {
 			System.out.println(numFives + " Fives");
 		}
-		
+
 		int numOnes = change / 100;// determines how many dollar bills
 		change = change % 100;// determines remaining change for next calculation
 		if (numOnes == 1) {
@@ -108,11 +109,6 @@ public class MoneyChanger {
 			System.out.printf("You are due $ %.2f in change from this transaction.", changeDue);// limits decimal
 																								// precision to 2
 																								// decimal places
-
-			// System.out.println("You should receive: " + numOnes + " $1's, " + numQuarters
-			// + " quarters, " + numDimes
-			// + " dimes, " + numNickels + " nickels, and " + numPennies + " pennies.");
-
 		}
 
 	}
